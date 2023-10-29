8910fec8e9c1e3529ce57b16ee51d20acaa02094
JOSUE <josueadrianib@gmail.com>
2023-10-28 22:48:22 -0500

    Adding class Student

---
+++ FinalProjectUniversity/src/com/globant/users/Student.java
@@ -0,0 +1,44 @@
+package com.globant.users;
+
+import java.util.Scanner;
+
+public class Student {
+    private String name;
+    private int id;
+    private int age;
+
+    public Student(String name, int id, int age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
+
+    public String getName() { return name; }
+
+    public static Student createNewStudent(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name of the new student:");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter the student ID: ");
+        int studentId = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter the age of the new student: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        Student newStudent = new Student(name, studentId, age);
+
+        //methods to get all the student
+
+    }
+
+    @Override
+    public String toString() {
+        return "-----------Student-----------\n" +
+                "\nName: " + name +
+                "\nID: " + id +
+                "\nAge: " + age + "\n\n\n";
+    }
+}
